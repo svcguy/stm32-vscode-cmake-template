bde9579a8f64518295316133720b2c5db54bd392
Andy <5302949+svcguy@users.noreply.github.com>
2021-02-02 15:25:43 -0600

    Changes to be committed:     new file:   .vscode/launch.json     new file:   .vscode/settings.json     new file:   CMakeLists.txt     new file:   inc/main.h     new file:   src/main.c

---
+++ inc/main.h
@@ -0,0 +1,27 @@
+/**
+ * @file main.h
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-02-02
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#ifndef _MAIN_H
+#define _MAIN_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif /* __cplusplus */
+
+#include "stm32g0xx.h"
+
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* _MAIN_H */
---
+++ src/main.c
@@ -0,0 +1,22 @@
+/**
+ * @file main.c
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-02-02
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include "main.h"
+
+int main(void)
+{
+    while(1)
+    {
+
+    }
+
+    return 1;
+}
